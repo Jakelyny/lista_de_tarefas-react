cb1ffffe62065705811e6f38f1afeeebcdfb1eed
Jakelyny Monroe <jakelynysousa.a@gmail.com>
2022-05-04 16:34:04 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
+import { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
+import { ListItem } from './components/ListItem'
+import { AddArea } from './components/AddArea';
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Comprar pão', done: false},
+    { id: 2, name: 'Comprar bolo', done: true},
+  ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
+
+  const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list];
+    for(let i in newList) {
+      if(newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  }
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
+
+        <AddArea onEnter={handleAddTask} />
+
+        {list.map((item, index) => (
+          <ListItem key={index} item={item} />
+        ))}
+
       </C.Area>
     </C.Container>
   );
